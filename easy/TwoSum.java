374ea8d9b5967772199455b542db01629e898da4
Ivan Chuvilin <i_chuvilin@outlook.com>
2022-12-01 19:49:11 +0700

    TowSum

---
+++ easy/TwoSum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> prev = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int n = target - nums[i];
+			if (prev.containsKey(nums[i])) {
+				return new int[] {prev.get(nums[i]), i};
+			} 
+			prev.put(n, i);
+		}
+		return null;
+    }
+}
