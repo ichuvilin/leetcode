04c101637e99e6162e9aaff9f1d9ff5f87ff9bee
Ivan Chuvilin <i_chuvilin@outlook.com>
2022-12-07 20:55:20 +0700

    Final Value of Variable After Performing Operations

---
+++ easy/FinalValueofVariableAfterPerformingOperations.java
@@ -0,0 +1,13 @@
+class FinalValueOfVariableAfterPerformingOperations {
+    public int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].equals("++X") || operations[i].equals("X++"))
+                result++;
+            else 
+                result--;
+        }
+
+        return result;
+    }
+}
